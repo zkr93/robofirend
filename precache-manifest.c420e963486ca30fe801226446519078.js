0718ecafcfce2476ac757b4a5e6d22393bb0395f
zkr93 <61614173+zkr93@users.noreply.github.com>
2020-04-23 07:08:29 +0530

    file added

---
+++ precache-manifest.c420e963486ca30fe801226446519078.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8beb03d9dc7e2ac546a3396e90534745",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0c83467aeffca9927d8",
+    "url": "/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "97084f7a45b9d9f6ea29",
+    "url": "/static/css/main.67cf1b16.chunk.css"
+  },
+  {
+    "revision": "a0c83467aeffca9927d8",
+    "url": "/static/js/2.546b5965.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/static/js/2.546b5965.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97084f7a45b9d9f6ea29",
+    "url": "/static/js/main.f7219f9d.chunk.js"
+  },
+  {
+    "revision": "932bfe5483626557dc9b",
+    "url": "/static/js/runtime-main.a958c9f3.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
